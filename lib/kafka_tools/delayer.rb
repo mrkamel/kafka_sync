cf2f29b209036e799d677985ef064a7ac9aa82f4
Benjamin Vetter <vetter@plainpicture.de>
2018-05-31 12:33:33 +0200

    Re-added logging to Delayer

--- lib/kafka_tools/delayer.rb
+++ lib/kafka_tools/delayer.rb
@@ -1,10 +1,10 @@
 
 module KafkaTools
   class Delayer
-    def initialize(topic:, name:, delay:, delay_topic: nil, consumer:, producer:)
+    def initialize(topic:, name:, delay:, delay_topic: nil, consumer:, producer:, logger: Logger.new("/dev/null"))
       consumer.consume(topic: topic, name: name) do |messages, concrete_consumer|
-        producer.batch do |batch|
-          messages.each_slice(250) do |slice|
+        messages.each_slice(250) do |slice|
+          producer.batch do |batch|
             slice.each do |message|
               hash = JSON.parse(message.value)
 
@@ -14,6 +14,8 @@ module KafkaTools
                 if batch.size > 0
                   batch.deliver
                   concrete_consumer.commit message.offset
+
+                  logger.debug "Delayed #{batch.size} messages for #{delay} seconds"
                 end
 
                 sleep(diff + 30) if diff > 0 
@@ -22,6 +24,8 @@ module KafkaTools
               batch.produce(JSON.generate(hash.merge("created_at" => Time.now.utc.to_f)), topic: delay_topic) if delay_topic
               batch.produce(JSON.generate(hash["payload"]), topic: hash["topic"])
             end
+
+            logger.debug("Delayed #{batch.size} messages for #{delay} seconds") if batch.size > 0
           end
         end
       end 
