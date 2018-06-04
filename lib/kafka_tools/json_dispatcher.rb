9929da6c8e0ff793b80e32420d72185027e7615c
Benjamin Vetter <vetter@plainpicture.de>
2018-06-04 15:07:44 +0200

    Changed JSONDispatcher

--- lib/kafka_tools/json_dispatcher.rb
+++ lib/kafka_tools/json_dispatcher.rb
@@ -1,23 +1,20 @@
 
 module KafkaTools
-  module JSONDispatcher
-    def initialize(consumer:, topic:, name:, logger: Logger.new("/dev/null"))
+  class JSONDispatcher
+    def initialize(consumer:, topic:, name:, target:, logger: Logger.new("/dev/null"))
       @consumer = consumer
       @topic = topic
       @name = name
       @logger = logger
+      @target = target
     end
 
     def run
       @consumer.consume(topic: @topic, name: @name) do |messages|
-        dispatch(messages.map { |message| message.parsed_json })
+        @target.dispatch(messages.map { |message| message.parsed_json })
 
         @logger.info "Dispatched #{messages.size} messages for #{@name}"
       end
-    end
-
-    def dispatch
-      raise NotImplementedError
     end
   end
 end
