ac374142c54eccf8bcd53250a1259d7dc846c653
Benjamin Vetter <vetter@plainpicture.de>
2018-06-19 13:49:36 +0200

    Added logger to KafkaTools::Barrier

--- lib/kafka_tools/barrier.rb
+++ lib/kafka_tools/barrier.rb
@@ -3,12 +3,14 @@ module KafkaTools
   class TimeoutError < StandardError; end
 
   class Barrier
-    def initialize(zk_hosts:, seed_brokers:, client_id: "kafka_tools", pool_size: 5, pool_timeout: 5)
+    def initialize(zk_hosts:, seed_brokers:, client_id: "kafka_tools", pool_size: 5, pool_timeout: 5, logger: Logger.new("/dev/null"))
       @zk_hosts = zk_hosts
 
       @kafka_pool = ConnectionPool.new(size: pool_size, timeout: pool_timeout) do
         Kafka.new(seed_brokers: seed_brokers, client_id: client_id)
       end
+
+      @logger = logger
     end
 
     def wait(topic:, name:, partition: 0, poll_interval: 1, timeout: 30)
@@ -17,7 +19,11 @@ module KafkaTools
       start_time = Time.now.to_f
 
       until zk.get("/kafka_tools/consumer/#{topic}/#{partition}/#{name}/offset")[0].to_i > offset
-        return false if Time.now.to_f - start_time > timeout
+        if Time.now.to_f - start_time > timeout
+          @logger.error "Barrier (#{topic}, #{partition}, #{name}) didn't finish within specified timeout (#{timeout})"
+
+          return false
+        end
 
         sleep poll_interval
       end
