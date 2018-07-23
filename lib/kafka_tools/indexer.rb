208f7f6e41d0227da1cfe4d6b6e22887e950f3c2
Benjamin Vetter <vetter@plainpicture.de>
2018-07-23 15:46:52 +0200

    Added batch_size option to Indexer

--- lib/kafka_tools/indexer.rb
+++ lib/kafka_tools/indexer.rb
@@ -1,32 +1,35 @@
 
 module KafkaTools
   class Indexer
-    def initialize(consumer:, topic:, name:, partition: 0, index:, logger: Logger.new("/dev/null"))
+    def initialize(consumer:, topic:, name:, partition: 0, index:, batch_size: 1_000, logger: Logger.new("/dev/null"))
       @consumer = consumer
       @topic = topic
       @name = name
       @partition = partition
       @index = index
       @logger = logger
+      @batch_size = batch_size
     end
 
     def run
       @consumer.consume(topic: @topic, name: @name, partition: @partition) do |messages|
-        indexed_messages = messages.index_by { |message| message.parsed_json["id"] }
-        ids = indexed_messages.keys
+        messages.each_slice(@batch_size) do |sliced_messages|
+          indexed_messages = sliced_messages.index_by { |message| message.parsed_json["id"] }
+          ids = indexed_messages.keys
 
-        @index.bulk ignore_errors: [409] do |bulk|
-          hash = @index.index_scope(@index.model.where(id: ids)).index_by(&:id)
+          @index.bulk ignore_errors: [409] do |bulk|
+            hash = @index.index_scope(@index.model.where(id: ids)).index_by(&:id)
 
-          ids.each do |id|
-            if object = hash[id]
-              bulk.import object.id, @index.serialize(object), @index.index_options(object)
-            else
-              bulk.delete id, @index.index_options(Hashie::Mash.new(indexed_messages[id].parsed_json))
+            ids.each do |id|
+              if object = hash[id]
+                bulk.import object.id, @index.serialize(object), @index.index_options(object)
+              else
+                bulk.delete id, @index.index_options(Hashie::Mash.new(indexed_messages[id].parsed_json))
+              end 
             end 
-          end 
 
-          @logger.info("#{@index.name} finished #{ids.count} messages") if ids.present?
+            @logger.info("#{@index.name} finished #{ids.count} messages") if ids.present?
+          end
         end
       end
     end
--- spec/kafka_tools/indexer_spec.rb
+++ spec/kafka_tools/indexer_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe KafkaTools::Indexer do
   it "should index the models when messages arrive" do
     ProductIndex.import(create(:product))
 
-    KafkaTools::Indexer.new(consumer: KafkaTools::Consumer.new, topic: "products", name: "product_indexer", index: ProductIndex).run
+    KafkaTools::Indexer.new(consumer: KafkaTools::Consumer.new, topic: "products", name: "product_indexer", index: ProductIndex, batch_size: 2).run
 
-    expect { create_list(:product, 2); sleep(1) }.to change { ProductIndex.total_count }.by(2)
+    expect { create_list(:product, 3); sleep(1) }.to change { ProductIndex.total_count }.by(3)
   end
 end
 
